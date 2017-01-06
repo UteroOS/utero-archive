e04614265df2bda89f851ad284c22ba48d398858
noriyotcp <noriyo.akita@gmail.com>
2017-01-07 04:14:22 +0900

    [main.cr] Display the result of memcmp

--- src/main.cr
+++ src/main.cr
@@ -8,8 +8,21 @@
 # except according to those terms.
 
 require "./arch/x86_64/scrn"
+require "./core/libcr"
 
 puts
 puts "Hello World from Crystal!!!"
 puts
 puts "Welcome to Utero!!!"
+puts
+
+puts "---------memcmp------------"
+puts "Comparing 'abcx' and 'abcv' returns 2"
+ptr1 = "abcx"
+ptr2 = "abcv"
+
+result = LibCR.memcmp(ptr1.as(Void*), ptr2.as(Void*), 4 * sizeof(String))
+puts "Displays 'memcmp' twice"
+result.times do
+  puts "memcmp"
+end

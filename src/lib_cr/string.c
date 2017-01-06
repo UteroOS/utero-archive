50ec439987d91c262df94d5856469632fbce55d9
noriyotcp <noriyo.akita@gmail.com>
2017-01-07 06:13:54 +0900

    Add license to files

--- src/lib_cr/string.c
+++ src/lib_cr/string.c
@@ -1,3 +1,9 @@
+/*
+The part of this file was taken from:
+musl https://www.musl-libc.org/
+Licensed under permissive MIT license
+*/
+
 #include "string.h"
 
 int memcmp(const void *vl, const void *vr, size_t n)

e21b07f0bd04d3b564d7873cbec8132b74d5200f
noriyotcp <noriyo.akita@gmail.com>
2017-01-07 04:12:18 +0900

    Add libraries for string and a generated static file (libcr.a)

---
+++ src/core/string.h
@@ -0,0 +1,18 @@
+#ifndef _STRING_H
+#define _STRING_H 1
+
+#include "stddef.h"
+
+int memcmp(const void* aptr, const void* bptr, size_t size);
+// void* memset(void* bufptr, int value, size_t size);
+// size_t strlen(const char*);
+// char* strcat(char* d, const char* s);
+// char* strcpy(char* d, const char* s);
+// int strcmp(const char * s1, const char * s2);
+// char *strncat(char *dest, const char *src, size_t n);
+// char *strncpy(char *dest, const char *src, size_t n);
+// char *strstr(char *s1, const char *s2);
+// char *strchr(const char *s, int c);
+// int strncmp(const char * s1, const char * s2, size_t n);
+// char *ctos(char s[2], const char c);
+#endif

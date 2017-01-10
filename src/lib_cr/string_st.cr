4f4bac0f248f66b52d78fc3743d830bc367890a8
noriyotcp <noriyo.akita@gmail.com>
2017-01-11 03:06:21 +0900

    Add an alternative implementation of strlen (commented out)

--- src/lib_cr/string_st.cr
+++ src/lib_cr/string_st.cr
@@ -2,4 +2,14 @@ struct StringST
   def self.strlen(str : UInt8*) : UInt64
     str[4].to_u64 # Byte size of str is in it
   end
+
+  # Alternative
+  # def self.strlen(str : UInt8*) : UInt64
+  #   ret = 0_u64
+  #   # str[12] seems to be the starting point of string
+  #   while str[12 + ret] != 0
+  #     ret += 1_u64
+  #   end
+  #   ret
+  # end
 end

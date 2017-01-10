c87015edb3d7f8a326222fffdb05c97b39f61b41
noriyotcp <noriyo.akita@gmail.com>
2017-01-11 00:54:13 +0900

    Change the type of return value on strlen from UInt8 to UInt64
    because Crystal's LibC.strlen returns UInt64

--- src/lib_cr/string_st.cr
+++ src/lib_cr/string_st.cr
@@ -1,5 +1,5 @@
 struct StringST
-  def self.strlen(str : UInt8*) : UInt8
-    str[4] # Byte size of str is in it
+  def self.strlen(str : UInt8*) : UInt64
+    str[4].to_u64 # Byte size of str is in it
   end
 end

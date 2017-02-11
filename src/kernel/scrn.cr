13eacdb247047b28e768cd3edc237413b69ecb9a
noriyotcp <noriyo.akita@gmail.com>
2017-02-11 23:57:37 +0900

    [scrn] Enable to set or reset color

--- src/kernel/scrn.cr
+++ src/kernel/scrn.cr
@@ -7,6 +7,7 @@
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 require "../arch/x86_64/io"
+require "./scrn_h"
 
 struct Scrn
   # Constants
@@ -15,11 +16,15 @@ struct Scrn
   private VGA_HEIGHT = 25
   private VGA_SIZE = VGA_WIDTH * VGA_HEIGHT
   private VGA_MEMORY = Pointer(UInt16).new(0xb8000_u64)
+  private VGA_FG_COLOR = VGAColor::LIGHT_GREEN.value
+  private VGA_BG_COLOR = VGAColor::BLACK.value
 
   def initialize
     @vmem = VGA_MEMORY
     @col = 0
     @row = 0
+    @vga_color = set_default_color.as(UInt8)
+
     VGA_WIDTH.times do |col|
       VGA_HEIGHT.times do |row|
         @vmem[row * VGA_WIDTH + col] = 0_u16
@@ -59,7 +64,6 @@ struct Scrn
   end
 
   def clear
-    # attr = make_attribute BLANK, @@color
     attr = 0x0f.to_u16 << 8 | ' '.ord
     VGA_SIZE.times { |i| @vmem[i] = attr }
 
@@ -72,7 +76,7 @@ struct Scrn
     @col = 0
     @row += 1
     scroll if @row == VGA_HEIGHT
-    # move_csr(@col, @row)
+    move_csr(@col, @row)
   end
 
   private def put_byte(byte)
@@ -92,9 +96,17 @@ struct Scrn
       return
     end
 
-    @vmem[@row * VGA_WIDTH + @col] = ((15_u16 << 8) | (0_u16 << 12) | byte).as(UInt16)
+    @vmem[@row * VGA_WIDTH + @col] = (@vga_color.to_u16 << 8 | byte).as(UInt16)
     @col += 1
     linebreak if @col == VGA_WIDTH
+  end
+
+  def set_default_color
+    @vga_color = VGA_BG_COLOR << 4 | VGA_FG_COLOR
+  end
+
+  def set_color(fc : VGAColor_T, bc : VGAColor_T) : VGAColor_T
+    @vga_color = bc << 4 | fc
   end
 
   def print(str)
@@ -123,4 +135,12 @@ end
 
 def clear
   SCRN.clear
+end
+
+def set_color(fc : VGAColor_T, bc : VGAColor_T)
+  SCRN.set_color(fc, bc)
+end
+
+def reset_color
+  SCRN.set_default_color
 end

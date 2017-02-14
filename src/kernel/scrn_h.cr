171450c79e05fc7f99977c63547b424e032f322d
noriyotcp <noriyo.akita@gmail.com>
2017-02-15 01:55:06 +0900

    Format the code by crystal tool format

--- src/kernel/scrn_h.cr
+++ src/kernel/scrn_h.cr
@@ -8,22 +8,22 @@
 # except according to those terms.
 
 enum VGAColor : UInt8
-  BLACK         #=> 0
-  BLUE          #=> 1
-  GREEN         #=> 2
-  CYAN          #=> 3
-  RED           #=> 4
-  MAGENTA       #=> 5
-  BROWN         #=> 6
-  LIGHT_GREY    #=> 7
-  DARK_GREY     #=> 8
-  LIGHT_BLUE    #=> 9
-  LIGHT_GREEN   #=> 10
-  LIGHT_CYAN    #=> 11
-  LIGHT_RED     #=> 12
-  LIGHT_MAGENTA #=> 13
-  LIGHT_BROWN   #=> 14
-  WHITE         #=> 15
+  BLACK         # => 0
+  BLUE          # => 1
+  GREEN         # => 2
+  CYAN          # => 3
+  RED           # => 4
+  MAGENTA       # => 5
+  BROWN         # => 6
+  LIGHT_GREY    # => 7
+  DARK_GREY     # => 8
+  LIGHT_BLUE    # => 9
+  LIGHT_GREEN   # => 10
+  LIGHT_CYAN    # => 11
+  LIGHT_RED     # => 12
+  LIGHT_MAGENTA # => 13
+  LIGHT_BROWN   # => 14
+  WHITE         # => 15
 end
 
 alias VGAColor_T = UInt8

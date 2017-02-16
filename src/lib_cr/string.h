677ca722ef08c938088011f112eacfebc589b7a4
noriyotcp <noriyo.akita@gmail.com>
2017-02-17 06:27:21 +0900

    [lib_cr/string] Sort functions by alphabetical order

--- src/lib_cr/string.h
+++ src/lib_cr/string.h
@@ -11,20 +11,24 @@ except according to those terms.
 
 #ifndef _STRING_H
 #define _STRING_H 1
-
 #include "stddef.h"
 
+/* Implemented (without *strerror)
+Uncommented are used in test_libc */
+void * memchr( const void * s, int c, size_t n );
 int memcmp(const void* aptr, const void* bptr, size_t size);
 void* memset(void* bufptr, int value, size_t size);
-// size_t strlen(const char*);
-// char* strcat(char* d, const char* s);
-// char* strcpy(char* d, const char* s);
+// char *strchr(const char *s, int c);
+// char *strchrnul(const char *s, int c)
 // int strcmp(const char * s1, const char * s2);
+// char *strerror(int e)
+// size_t strlen(const char*);
+// int strncmp(const char * s1, const char * s2, size_t n);
+// char* strcat(char* d, const char* s);
+// char *strstr(char *s1, const char *s2);
+
+/* Not Implemented yet */
+// char* strcpy(char* d, const char* s);
 // char *strncat(char *dest, const char *src, size_t n);
 // char *strncpy(char *dest, const char *src, size_t n);
-// char *strstr(char *s1, const char *s2);
-// char *strchr(const char *s, int c);
-// int strncmp(const char * s1, const char * s2, size_t n);
-void * memchr( const void * s, int c, size_t n );
-// char *ctos(char s[2], const char c);
 #endif

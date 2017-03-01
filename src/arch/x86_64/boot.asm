36f06c53daf9193c4c9c14d7410540b0a7c0d711
noriyotcp <noriyo.akita@gmail.com>
2017-03-02 01:55:32 +0900

    [boot.asm] Remove ignored flag for 64bit gdt

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -197,7 +197,7 @@ section .rodata
 gdt64:
 	dq 0 ; zero entry
 .code: equ $ - gdt64
-	dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
+	dq (1<<44) | (1<<47) | (1<<43) | (1<<53) ; code segment
 .data: equ $ - gdt64
 	dq (1<<44) | (1<<47) | (1<<41) ; data segment
 .pointer:

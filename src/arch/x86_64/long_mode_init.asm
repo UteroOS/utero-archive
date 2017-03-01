2d22cfb4d43ca94cea35de830e946b9678d4b971
noriyotcp <noriyo.akita@gmail.com>
2017-03-02 02:01:07 +0900

    [long_mode_init.asm] Load 0 into all data segment registors before calling main

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -16,6 +16,14 @@ extern main
 section .text
 bits 64
 long_mode_start:
+    ; load 0 into all data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
     ; call main.cr
     call main
 

af875860a8fce08cdaa69095ca4bca384b2a3dec
noriyotcp <noriyo.akita@gmail.com>
2017-03-06 00:18:31 +0900

    [io.cr] Implement inb function

--- src/arch/x86_64/io.cr
+++ src/arch/x86_64/io.cr
@@ -2,3 +2,10 @@
 def outb(port : UInt16, val : UInt8)
   asm("outb $1, $0" :: "{dx}"(port), "{al}"(val) :: "volatile")
 end
+
+@[AlwaysInline]
+def inb(port : UInt16) : UInt8
+  val = 0_u8
+  asm("inb $1, $0" : "={al}"(val) : "{dx}"(port) :: "volatile")
+  val
+end

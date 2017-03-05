7df629fec3529345a3f866ede39e27a4019e7ea7
noriyotcp <noriyo.akita@gmail.com>
2017-03-06 00:17:54 +0900

    [io.cr] Add AlwaysInline attribute and remove a comment

--- src/arch/x86_64/io.cr
+++ src/arch/x86_64/io.cr
@@ -1,4 +1,4 @@
+@[AlwaysInline]
 def outb(port : UInt16, val : UInt8)
-  # __asm__ volatile ("outb %0,%1" : : "a" (__val), "dN" (__port));
   asm("outb $1, $0" :: "{dx}"(port), "{al}"(val) :: "volatile")
 end

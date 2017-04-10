4572366e9ac0c61364d4891d9d8df6613771aaec
noriyotcp <noriyo.akita@gmail.com>
2017-04-10 20:14:14 +0900

    [idt.h] Add constants

--- src/arch/x86_64/c/idt.h
+++ src/arch/x86_64/c/idt.h
@@ -15,6 +15,33 @@
 
 #include "stddef.h"
 
+// This bit shall be set to 0 if the IDT slot is empty
+#define IDT_FLAG_PRESENT 	0x80
+// Interrupt can be called from within RING0
+#define IDT_FLAG_RING0		0x00
+// Interrupt can be called from within RING1 and lower
+#define IDT_FLAG_RING1		0x20
+// Interrupt can be called from within RING2 and lower
+#define IDT_FLAG_RING2		0x40
+// Interrupt can be called from within RING3 and lower
+#define IDT_FLAG_RING3		0x60
+// Size of gate is 16 bit
+#define IDT_FLAG_16BIT		0x00
+// Size of gate is 32 bit
+#define IDT_FLAG_32BIT		0x08
+// The entry describes an interrupt gate
+#define IDT_FLAG_INTTRAP	0x06
+// The entry describes a trap gate
+#define IDT_FLAG_TRAPGATE	0x07
+// The entry describes a task gate
+#define IDT_FLAG_TASKGATE	0x05
+
+/*
+ * This is not IDT-flag related. It's the segment selectors for kernel code and data.
+ */
+#define KERNEL_CODE_SELECTOR	0x08
+#define KERNEL_DATA_SELECTOR	0x10
+
 typedef struct {
   // Handler function's lower 16 address bits
   uint16_t base_lo;
@@ -33,9 +60,9 @@ typedef struct {
 } __attribute__ ((packed)) idt_entry_t;
 
 typedef struct {
-  /// Size of the IDT in bytes (not the number of entries!)
+  // Size of the IDT in bytes (not the number of entries!)
   uint16_t limit;
-  /// Base address of the IDT
+  // Base address of the IDT
   size_t base;
 } __attribute__ ((packed)) idt_ptr_t;
 

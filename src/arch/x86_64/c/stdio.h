d0950263cca4e4d521f4a543321cb7ece0007ff1
noriyotcp <noriyo.akita@gmail.com>
2017-04-10 20:19:50 +0900

    Add required header files

---
+++ src/arch/x86_64/c/stdio.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2016-2017 Utero OS Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// Functions are taken from musl/include/stdio.h
+
+#ifndef STDIO_H
+#define STDIO_H
+
+int sprintf(char *__restrict, const char *__restrict, ...);
+
+#endif /* end of include guard: STDIO_H */

1a6292337f5b65d263d93a398ae3b8b68a4a1d2c
noriyotcp <noriyo.akita@gmail.com>
2017-04-15 08:48:23 +0900

    [multiboot_header] Fix typo and align the beginning of comments

--- src/arch/x86_64/multiboot_header.asm
+++ src/arch/x86_64/multiboot_header.asm
@@ -12,9 +12,9 @@
 
 section .multiboot_header
 header_start:
-	dd 0xe85250d6					; magic number (multiboot 2)
-	dd 0							; architecture 0 (protected mode i386)
-	dd header_end - header_start	; heade length
+	dd 0xe85250d6									; magic number (multiboot 2)
+	dd 0													; architecture 0 (protected mode i386)
+	dd header_end - header_start	; header length
 	; checksum
 	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 

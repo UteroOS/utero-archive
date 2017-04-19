f42ba4fd6176fa0869d64200abf462658335dac2
noriyotcp <noriyo.akita@gmail.com>
2017-04-19 16:36:38 +0900

    Add stdint.h (imcompleted) and add constants to bits/alltypes.h

--- src/arch/x86_64/c/bits/alltypes.h
+++ src/arch/x86_64/c/bits/alltypes.h
@@ -9,8 +9,20 @@
 //
 // The part of this file was taken from:
 // musl/obj/include/bits/alltypes.h
+#define _Addr long
+#define _Int64 long
+#define _Reg long
 
 #if defined(__NEED_va_list) && !defined(__DEFINED_va_list)
 typedef __builtin_va_list va_list;
 #define __DEFINED_va_list
 #endif
+
+#if defined(__NEED_uintptr_t) && !defined(__DEFINED_uintptr_t)
+typedef unsigned _Addr uintptr_t;
+#define __DEFINED_uintptr_t
+#endif
+
+#undef _Addr
+#undef _Int64
+#undef _Reg

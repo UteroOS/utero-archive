04515c4bca97914f05bc62679ce0fd8fdfbee49a
noriyotcp <noriyo.akita@gmail.com>
2017-05-08 10:35:44 +0900

    Enable to refer to linker symbols directly from C
    add bss_start and bss_end, comment out early_info

--- src/arch/x86_64/c/utero_init.c
+++ src/arch/x86_64/c/utero_init.c
@@ -4,25 +4,34 @@
 #define N 256
 
 // Parameters passed from assembly or linker
-unsigned int kernel_start;
-unsigned int kernel_end;
+extern const void * kernel_start;
+extern const void * kernel_end;
+extern const void * bss_start;
+extern const void * bss_end;
 
-int early_info(unsigned int ks, unsigned int ke)
-{
-  kernel_start = ks;
-  kernel_end = ke;
+// int early_info(unsigned int ks, unsigned int ke)
+// {
+  // kernel_start = ks;
+  // kernel_end = ke;
 
-  return 0;
-}
+  // return 0;
+// }
 
 char *make_kernel_info()
 {
   static char *str[N];
-  char *fmt = "Kernel starts at: %p\nKernel ends   at: %p\nmultiboot info at:%p\n";
+  char *fmt = "\
+Kernel starts  at: %p\n\
+Kernel ends    at: %p\n\
+Bss starts     at: %p\n\
+Bss ends       at: %p\n\
+multiboot info at: %p\n";
 
   *str = make_string(fmt,
                      &kernel_start,
                      &kernel_end,
+                     &bss_start,
+                     &bss_end,
                      &mb_info);
   return *str;
 }
